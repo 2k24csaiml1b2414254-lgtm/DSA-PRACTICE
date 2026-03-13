f4a29b1dffb81553bcacdf6bfbac943782ed2aaa
Himesh Mishra <2k24.csaiml1b.2414254@gmail.com>
2026-03-13 23:39:39 +0530

    approach update
    Added problem statement and approach details as comments.

--- 01- Arrays/02_Best Time to Buy and Sell Stock.java
+++ 01- Arrays/02_Best Time to Buy and Sell Stock.java
@@ -1,1 +1,11 @@
+/*
+Problem: Best Time to Buy and Sell Stock
+Platform: LeetCode
+Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+Approach:
+Track minimum price so far and calculate maximum profit.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
