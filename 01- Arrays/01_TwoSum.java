042f721400818a944b384a968324662e693fd416
Himesh Mishra <2k24.csaiml1b.2414254@gmail.com>
2026-03-13 22:42:33 +0530

    Add comments for Two Sum problem explanation
    Added problem statement and approach details for Two Sum.

--- 01- Arrays/01_TwoSum.java
+++ 01- Arrays/01_TwoSum.java
@@ -1,1 +1,11 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Link: https://leetcode.com/problems/two-sum/
 
+Approach:
+Use HashMap to store visited numbers and their indices.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
