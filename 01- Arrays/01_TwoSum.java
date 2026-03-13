b2b8fdba0b34fb2cb2f20bad996e6f52b71b192f
Himesh Mishra <2k24.csaiml1b.2414254@gmail.com>
2026-03-13 23:29:13 +0530

    code updated
    Implement twoSum method to find indices of two numbers that add up to the target.

--- 01- Arrays/01_TwoSum.java
+++ 01- Arrays/01_TwoSum.java
@@ -9,3 +9,24 @@ Use HashMap to store visited numbers and their indices.
 Time Complexity: O(n)
 Space Complexity: O(n)
 */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] array = new int[2];
+        int idx1 = 0;
+        int idx2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                idx1 = map.get(complement);
+                idx2 = i;
+                array[0] = idx1;
+                array[1] = idx2;
+                return array;
+            }
+            map.put(nums[i], i);
+        }
+        return array;
+    }
+
+}
