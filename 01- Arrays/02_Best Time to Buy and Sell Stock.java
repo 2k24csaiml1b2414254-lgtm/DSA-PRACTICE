6a98e42ca4a7cba844812df165d856aa2707a683
Himesh Mishra <2k24.csaiml1b.2414254@gmail.com>
2026-03-14 09:29:58 +0530

    code updated

--- 01- Arrays/02_Best Time to Buy and Sell Stock.java
+++ 01- Arrays/02_Best Time to Buy and Sell Stock.java
@@ -9,3 +9,19 @@ Track minimum price so far and calculate maximum profit.
 Time Complexity: O(n)
 Space Complexity: O(1)
 */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            int profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+}
